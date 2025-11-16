0560f61ee73f02796e2048eb05ac348831faf002
Chandra Shekhar <cshekhar017@gmail.com>
2025-11-16 11:49:44 +0530

    Added backend to stream metrics in every 1 second
    Uses Math.Random() to generate random number for cpu,memory and error

---
+++ backend/src/server.ts
@@ -0,0 +1,46 @@
+import path from "path";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+let serviceCount = 5;
+
+type Metric = {
+  serviceName: string;
+  cpu: number;
+  memory: number;
+  errorRate: number;
+};
+
+function createMetrics(name: string): Metric {
+  return {
+    serviceName: name,
+    cpu: Math.floor(Math.random() * 100),
+    memory: Math.floor(Math.random() * 100),
+    errorRate: Number((Math.random() * 8).toFixed(1))
+  };
+}
+
+app.get("/metrics/stream", (req, res) => {
+  res.setHeader("Content-Type", "text/event-stream");
+  res.setHeader("Cache-Control", "no-cache");
+  res.flushHeaders();
+
+  const sendData = () => {
+    const list: Metric[] = [];
+    for (let i = 1; i <= serviceCount; i++) {
+      list.push(createMetrics(`service-${i}`));
+    }
+    res.write(`data: ${JSON.stringify(list)}\n\n`);
+  };
+
+  setInterval(sendData, 1000);
+
+});
+
+const PORT = 4000;
+app.listen(PORT, () => {
+  console.log("Backend running on port", PORT);
+});
